8a52a7e256c50b27a3b7647b8675a66074703f6a
cbmob0329 <pb2matsuyamadance@gmail.com>
2026-08-09 20:05:18 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,247 @@
+const MOB_DATA = {
+  players: [
+    {
+      id: "yusha",
+      name: "モブ勇者",
+      image: "play/01.png",
+      symbol: "勇",
+      attribute: "光",
+      skill: "星降りの一振り",
+      skillType: "physical",
+      skillPower: 1.65
+    },
+    {
+      id: "pink",
+      name: "モブピンク",
+      image: "play/02.png",
+      symbol: "桃",
+      attribute: "無",
+      skill: "キングダムソルジャー",
+      skillType: "physical",
+      skillPower: 1.5
+    },
+    {
+      id: "sand",
+      name: "モブサンド",
+      image: "play/03.png",
+      symbol: "砂",
+      attribute: "地",
+      skill: "スナノサバキ",
+      skillType: "physical",
+      skillPower: 1.8
+    },
+    {
+      id: "nyoro",
+      name: "モブニョロ",
+      image: "play/04.png",
+      symbol: "炎",
+      attribute: "火",
+      skill: "マグケロキングダム",
+      skillType: "magic",
+      skillPower: 1.65
+    },
+    {
+      id: "nekoku",
+      name: "モブネコクー",
+      image: "play/05.png",
+      symbol: "水",
+      attribute: "水",
+      skill: "ウォーターキル・ザ・ビート",
+      skillType: "magic",
+      skillPower: 1.85
+    },
+    {
+      id: "jerry",
+      name: "モブジェリー",
+      image: "play/06.png",
+      symbol: "雷",
+      attribute: "雷",
+      skill: "クライマックスチェイス",
+      skillType: "physical",
+      skillPower: 1.7
+    },
+    {
+      id: "denden",
+      name: "モブデンデン",
+      image: "play/07.png",
+      symbol: "電",
+      attribute: "雷",
+      skill: "デンデンサンダーボルト",
+      skillType: "magic",
+      skillPower: 1.85
+    },
+    {
+      id: "money",
+      name: "モブマニー",
+      image: "play/08.png",
+      symbol: "光",
+      attribute: "光",
+      skill: "レトロミラージュマニー",
+      skillType: "magic",
+      skillPower: 1.45
+    },
+    {
+      id: "riro",
+      name: "モブリーロ",
+      image: "play/09.png",
+      symbol: "風",
+      attribute: "風",
+      skill: "リーロ・トゥ・ステイシー",
+      skillType: "physical",
+      skillPower: 1.8
+    },
+    {
+      id: "tetsu",
+      name: "モブテツ",
+      image: "play/10.png",
+      symbol: "鉄",
+      attribute: "地",
+      skill: "鉄の極意",
+      skillType: "physical",
+      skillPower: 1.9
+    },
+    {
+      id: "lilith",
+      name: "モブリリス",
+      image: "play/11.png",
+      symbol: "薔",
+      attribute: "闇",
+      skill: "ローズ・ウォール・ストリート",
+      skillType: "magic",
+      skillPower: 1.85
+    },
+    {
+      id: "naraku",
+      name: "モブナラク",
+      image: "play/12.png",
+      symbol: "魔",
+      attribute: "闇",
+      skill: "マスター・オブ・ピラミッド",
+      skillType: "magic",
+      skillPower: 2.0
+    }
+  ],
+
+  monsters: [
+    {
+      id: "hawk",
+      name: "モブホーク",
+      image: "",
+      symbol: "鷹",
+      attribute: "風",
+      skill: "ホークダイブ",
+      skillType: "physical",
+      skillPower: 1.35
+    },
+    {
+      id: "mira",
+      name: "ミラモブ",
+      image: "",
+      symbol: "毒",
+      attribute: "闇",
+      skill: "ミラモブポイズン",
+      skillType: "physical",
+      skillPower: 1.45
+    },
+    {
+      id: "guardian",
+      name: "モブガーディアン",
+      image: "",
+      symbol: "盾",
+      attribute: "地",
+      skill: "ガーディアンシールド",
+      skillType: "mental",
+      skillPower: 1.25
+    },
+    {
+      id: "neon",
+      name: "モブネオン",
+      image: "",
+      symbol: "光",
+      attribute: "光",
+      skill: "ネオンボム",
+      skillType: "magic",
+      skillPower: 1.55
+    },
+    {
+      id: "dragon",
+      name: "モブドラゴン",
+      image: "",
+      symbol: "竜",
+      attribute: "火",
+      skill: "ドラゴンフレイム",
+      skillType: "magic",
+      skillPower: 1.7
+    },
+    {
+      id: "nepu",
+      name: "モブネプ",
+      image: "",
+      symbol: "海",
+      attribute: "水",
+      skill: "ネプチューン・トライデント",
+      skillType: "physical",
+      skillPower: 1.7
+    },
+    {
+      id: "berserk",
+      name: "モブバーサク",
+      image: "",
+      symbol: "岩",
+      attribute: "地",
+      skill: "デストロイボム",
+      skillType: "physical",
+      skillPower: 1.65
+    },
+    {
+      id: "dendenBoss",
+      name: "モブデンデン",
+      image: "",
+      symbol: "電",
+      attribute: "雷",
+      skill: "マシンガングミ",
+      skillType: "physical",
+      skillPower: 1.55
+    },
+    {
+      id: "gidora",
+      name: "モブギドラ",
+      image: "",
+      symbol: "龍",
+      attribute: "火",
+      skill: "フル・ドラゴンフレイム",
+      skillType: "magic",
+      skillPower: 1.95
+    },
+    {
+      id: "maou",
+      name: "モブ魔王",
+      image: "",
+      symbol: "王",
+      attribute: "闇",
+      skill: "マスター・オブ・ピラミッド",
+      skillType: "magic",
+      skillPower: 2.05
+    },
+    {
+      id: "smith",
+      name: "モブスミス",
+      image: "",
+      symbol: "眼",
+      attribute: "風",
+      skill: "ゴールデン・アイ",
+      skillType: "physical",
+      skillPower: 1.9
+    },
+    {
+      id: "enma",
+      name: "モブ閻魔",
+      image: "",
+      symbol: "閻",
+      attribute: "闇・火",
+      skill: "ヒノカグヅチ",
+      skillType: "magic",
+      skillPower: 2.15
+    }
+  ]
+};
